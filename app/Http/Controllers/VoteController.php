024e0acd1254d9243e673f737c836584d72c14f0
Paige Saunders <psaunders@sosnewmedia.com>
2015-05-12 12:02:01 -0600

    Setting up basic unit testing

--- app/Http/Controllers/VoteController.php
+++ app/Http/Controllers/VoteController.php
@@ -7,6 +7,15 @@ use Illuminate\Http\Request;
 
 class VoteController extends Controller {
 
+	protected $rules = [
+		'motion_id' 	=>	'integer',
+		'position'		=>	'integer'
+	];
+
+	public function rules(){
+		return $this->rules;
+    }
+
 	/**
 	 * Display a listing of the resource.
 	 *
@@ -24,7 +33,19 @@ class VoteController extends Controller {
 	 */
 	public function create()
 	{
-		//
+		if(Auth::user()->can('create-vote')){
+			$input = Request::all();
+			$validator = Validator::make($input,$this->rules);
+			if($validator->fails()){
+				return $validator->messages();
+			} else {
+				$vote = Vote::firstOrNew(['motion_id'=>$input['motion_id'],'user_id'=>Auth::user()->id]);
+				$vote->position = $input['position'];
+				$vote->save();
+			}
+		} else {
+			return array('message'=>'You do not have permission to place a vote');
+		}
 	}
 
 	/**
