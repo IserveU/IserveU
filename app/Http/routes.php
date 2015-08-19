62dd646ba5ea93f50897320e11d80f6e043adac0
jalafel <jessica.quynh.tran@gmail.com>
2015-08-19 14:42:35 -0600

    added property controllers to routes

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -65,10 +65,16 @@ Route::group(array('prefix' => 'api'), function(){
 		Route::resource('user.comment', 'UserCommentController'); //, ['only'=>['index']]);
 
 		Route::post('property/uploadcsv', 'PropertyController@uploadCSV');
-		Route::resource('property','PropertyController');
 
 		Route::group(['middleware' => 'role:administrator'], function(){
  			Route::resource('propertyassessment', 'PropertyAssessmentController');
+ 			Route::resource('propertydescription', 'PropertyDescriptionController');
+ 			Route::resource('propertypolldivision', 'PropertyPollDivisionController');
+ 			Route::resource('propertycoordinate', 'PropertyCoordinateController');
+ 			Route::resource('propertyblock', 'PropertyBlockController');
+ 			Route::resource('propertyplan', 'PropertyPlanController');
+ 			Route::resource('propertyzone', 'PropertyZoneController');
+ 			Route::resource('property', 'PropertyController');
  		});
 	});
 });
