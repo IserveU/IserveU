767e2e298ca01e7d993a584504c92b0d8056c2f9
jalafel <jessica.quynh.tran@gmail.com>
2015-08-26 14:43:18 -0600

    changed permissions to a multidimensional array

--- app/User.php
+++ app/User.php
@@ -202,9 +202,8 @@ class User extends ApiModel implements AuthenticatableContract, CanResetPassword
 		foreach ($this->roles as $role){
 			$role_permissions = $role->perms()->get();
 			foreach($role_permissions as $permission){
-				if(!in_array($permission->name,$permissions)){
-					$permissions[]=$permission->name;
-				}
+				$newarray = explode('-', $permission->name);
+				$permissions[$newarray[1]][] = $newarray[0];
 			}
 		}
 		return $permissions;
