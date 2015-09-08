d2faa4de0626a9f475b9d91ee0fcc407d3c55916
jalafel <jessica.quynh.tran@gmail.com>
2015-09-08 15:24:57 -0600

    directive to check if the password matches; re-usable for other forms

---
+++ public/app/shared/resetpassword/passwordmatch.dir.js
@@ -0,0 +1,36 @@
+(function() {
+	
+	'use strict';
+
+	angular
+		.module('iserveu')
+		.directive('compareTo', compareTo);
+
+	function compareTo($compile) {
+
+		function linkMethod(scope, element, attrs, ngModel) {
+
+			console.log('here at link');
+
+			ngModel.$validators.compareTo = function(modelValue) {
+				return modelValue == scope.otherModelValue;
+			};
+
+			scope.$watch("otherModelValue", function() {
+				ngModel.$validate();
+			});
+		}
+		
+		return {
+			restrict: 'AE',
+			require: "^ngModel",
+			scope: {
+				otherModelValue: "=compareTo"
+			},
+			replace: true,
+			link: linkMethod
+		}
+
+	}
+
+}());
