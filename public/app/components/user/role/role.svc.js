03717c924fa0bed23184aabae11573bf662041ab
jalafel <jessica.quynh.tran@gmail.com>
2015-09-11 16:43:18 -0600

    removed console logs

--- public/app/components/user/role/role.svc.js
+++ public/app/components/user/role/role.svc.js
@@ -22,16 +22,13 @@
 		}
 
 		vm.grant = function(role_name, user_id){
-
-			console.log(role_name);
-			console.log(user_id);
 			var data = {
 				user_id: user_id,
 				role_name: role_name
 			}
 
 			role.grantRole(data).then(function(results){
-				console.log(results);
+				
 			}, function(error){
 				console.log(error);
 			})
