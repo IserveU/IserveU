9281537b6f17af5108635d51cac727289f426b3d
Paige Saunders <psaunders@sosnewmedia.com>
2015-10-01 14:16:07 -0600

    Making migration setup motion ranks

--- database/migrations/2015_09_30_184714_create_motion_ranks_table.php
+++ database/migrations/2015_09_30_184714_create_motion_ranks_table.php
@@ -2,6 +2,10 @@
 
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
+use App\Vote;
+
+use App\Events\VoteUpdated; //Move to migration
+
 
 class CreateMotionRanksTable extends Migration
 {
@@ -12,6 +16,8 @@ class CreateMotionRanksTable extends Migration
      */
     public function up()
     {
+        \Auth::loginUsingId(1);
+
         Schema::create('motion_ranks', function(Blueprint $table) {
             $table->increments('id');
             $table->integer('rank');
@@ -19,6 +25,13 @@ class CreateMotionRanksTable extends Migration
             $table->foreign('motion_id')->references('id')->on('motions');
             $table->timestamps();
         });
+
+
+        $votes = Vote::all(); //Generate the motion ranks so far
+        foreach($votes as $vote){
+            event(new VoteUpdated($vote));
+        }
+
     }
 
     /**
