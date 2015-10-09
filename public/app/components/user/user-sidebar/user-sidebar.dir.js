8576cd594309e0021cb9300354c584ed3af2902a
jalafel <jessica.quynh.tran@gmail.com>
2015-10-09 00:38:06 -0600

    user profile ui fixes

--- public/app/components/user/user-sidebar/user-sidebar.dir.js
+++ public/app/components/user/user-sidebar/user-sidebar.dir.js
@@ -8,20 +8,12 @@
 
   function userSidebar($rootScope, SetPermissionsService) {
 
-  	if(!SetPermissionsService.can('administrate-users')){
   	return {
 
-      templateUrl: 'app/components/motion/motion-sidebar/motion-sidebar.tpl.html'
+      templateUrl: SetPermissionsService.can('administrate-users') ? 'app/components/user/user-sidebar/user-sidebar.tpl.html' :'app/components/motion/motion-sidebar/motion-sidebar.tpl.html'
       
       }
 
   	}
-    else if(SetPermissionsService.can('administrate-users')){
-    return {
 
-      templateUrl: 'app/components/user/user-sidebar/user-sidebar.tpl.html'
-            
-    }
-	}
-  }
 })();
