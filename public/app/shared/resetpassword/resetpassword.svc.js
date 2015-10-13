841e21ff14ee4c9484f792664e51ec17bdc6c8bc
jalafel <jessica.quynh.tran@gmail.com>
2015-10-13 15:54:39 -0600

    refactored reset password function

--- public/app/shared/resetpassword/resetpassword.svc.js
+++ public/app/shared/resetpassword/resetpassword.svc.js
@@ -12,12 +12,12 @@
 		var vm = this;
 
 		vm.check = function(){
+			
 			if($state.current.name == 'login.resetpassword'){
 				auth.getNoPassword($stateParams.resetpassword).then(function(data) {
+					
 					afterauth.setLoginAuthDetails(data, data.data.token);
-					$timeout(function() {
-						$rootScope.$emit('resetPasswordNotification');
-					}, 1000);
+
 				}, function(error) {
 					if(error.status === 404){
 						console.log('invalid token');
