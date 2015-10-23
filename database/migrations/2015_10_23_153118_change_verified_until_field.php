528fc9642734d026c122e81c02e9708f2fe3a726
Paige Saunders <psaunders@sosnewmedia.com>
2015-10-23 15:36:03 -0600

    Fixes confusing field name

---
+++ database/migrations/2015_10_23_153118_change_verified_until_field.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class ChangeVerifiedUntilField extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('users', function($table){
+            $table->dropColumn('verified_until');
+            $table->date('address_verified_until')->nullable();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        //
+    }
+}
