226a4c065d4da91d1b139940d52e8223ef4e636c
jalafel <jessica.quynh.tran@gmail.com>
2015-10-26 16:17:48 -0600

    background image bar permissions

--- public/app/components/backgroundimage/backgroundimage-sidebar/backgroundimage-sidebar.dir.js
+++ public/app/components/backgroundimage/backgroundimage-sidebar/backgroundimage-sidebar.dir.js
@@ -7,11 +7,11 @@
     .directive('backgroundimageSidebar', backgroundimageSidebar)
     .directive('backgroundimage.previewSidebar', backgroundimagePreviewSidebar);
 
-  function backgroundimageSidebar() {
+  function backgroundimageSidebar(SetPermissionsService) {
 
     return {
 
-      templateUrl: 'app/components/backgroundimage/backgroundimage-sidebar/backgroundimage-sidebar.tpl.html'
+      templateUrl: SetPermissionsService.can('administrate-background_images') ? 'app/components/backgroundimage/backgroundimage-sidebar/backgroundimage-sidebar.tpl.html' :'app/components/motion/motion-sidebar/motion-sidebar.tpl.html'
       
     }
   }
@@ -20,7 +20,7 @@
 
     return {
 
-      templateUrl: 'app/components/backgroundimage/backgroundimage-sidebar/backgroundimage-sidebar.tpl.html'
+      templateUrl: SetPermissionsService.can('administrate-background_images') ? 'app/components/backgroundimage/backgroundimage-sidebar/backgroundimage-sidebar.tpl.html' :'app/components/motion/motion-sidebar/motion-sidebar.tpl.html'
       
     }
   }
