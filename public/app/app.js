485ffd3a4ca07dcfbbbd2060786b3eb09a9a79e5
jalafel <jessica.quynh.tran@gmail.com>
2015-10-27 16:40:52 -0600

    displays file size

--- public/app/app.js
+++ public/app/app.js
@@ -96,6 +96,15 @@
 		    	return Object.keys(obj).map(function(key){return obj[key];});
 		    }
 	 	})
+	 	.filter('bytes', function() {
+			return function(bytes, precision) {
+				if (isNaN(parseFloat(bytes)) || !isFinite(bytes)) return '-';
+				if (typeof precision === 'undefined') precision = 1;
+				var units = ['bytes', 'kB', 'MB', 'GB', 'TB', 'PB'],
+					number = Math.floor(Math.log(bytes) / Math.log(1024));
+				return (bytes / Math.pow(1024, Math.floor(number))).toFixed(precision) +  ' ' + units[number];
+			}
+		})
 		.run(function($rootScope, $auth, $state, auth, $window) {
 
 			$rootScope.themename = 'default';
