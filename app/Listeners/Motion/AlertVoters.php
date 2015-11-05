ec0cdfbc43cd86f4d05124f3c4a1426b26f561c3
Paige Saunders <psaunders@sosnewmedia.com>
2015-11-05 11:27:44 -0700

    Send voter alert email when motion is changed

---
+++ app/Listeners/Motion/AlertVoters.php
@@ -0,0 +1,54 @@
+<?php
+
+namespace App\Listeners\Motion;
+
+use App\Vote;
+use App\Events\MotionUpdated;
+use Illuminate\Queue\InteractsWithQueue;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Mail;
+
+class AlertVoters
+{
+    /**
+     * Create the event listener.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        //
+    }
+
+    /**
+     * Handle the event. Wonder if this could all just be a daily email if the "Updated" field has changed
+     *
+     * @param  MotionUpdated  $event
+     * @return void
+     */
+    public function handle(MotionUpdated $event)
+    {
+        $motion = $event->motion;
+
+        $changedFields = $motion->getAlteredLockedFields();
+
+        if(!empty($changedFields)){
+
+            $motionVotes = Vote::whereHas('user',function($query){
+                $query->whereNull('deleted_at');
+            })->where('motion_id',$motion->id)->get();
+
+            foreach($motionVotes as $motionVote){
+                $data = array(
+                    'user'      =>  $motionVote->user,
+                    'motion'    =>  $motion
+                );
+
+                Mail::send('emails.motionchanged',$data, function ($m) use ($motionVote) {
+                    $m->to($motionVote->user->email, $motionVote->user->first_name.' '.$motionVote->user->last_name)->subject('A Motion You Voted On Has Changed');
+                });
+            }
+
+        }
+    }
+}
