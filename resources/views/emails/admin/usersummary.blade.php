1376e015de30a8562038dd0ef446c2e2a98c8432
Paige Saunders <psaunders@sosnewmedia.com>
2015-11-15 17:33:05 -0700

    Formatting emails with updated Settings array

--- resources/views/emails/admin/usersummary.blade.php
+++ resources/views/emails/admin/usersummary.blade.php
@@ -6,7 +6,7 @@
 @if(!$newUsers->isEmpty())
 	<h2>New Users</h2>
 	@foreach($newUsers as $newUser)
-		<p><a href="<?=base_path('users/'.$newUser->id)?>">{{$newUser->first_name}} {{$newUser->last_name}}</a></p>
+		<p><a href="<?=url()?>#/users/<?=$newUser->id?>">{{$newUser->first_name}} {{$newUser->last_name}}</a></p>
 	@endforeach
 @endif
 
