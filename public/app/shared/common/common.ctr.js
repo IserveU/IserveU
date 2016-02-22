998395a372708fb8330f7b291a7655b1b9aa595c
jalafel <jessica.quynh.tran@gmail.com>
2016-02-22 18:14:29 -0500

    logo grab

--- public/app/shared/common/common.ctr.js
+++ public/app/shared/common/common.ctr.js
@@ -10,6 +10,16 @@
 
 		this.settings = settings.getData();
 
+		this.getLogoUrl = function() {
+
+			console.log('foo');
+
+			return this.settings.theme.logo == 'default' 
+				   ? '/themes/default/logo/symbol_mono.svg'
+				   : this.settings.theme.logo;
+
+		}
+
 	}
 
 
