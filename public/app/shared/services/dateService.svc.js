64ede5f2d8b6b909f9d868248a89b360f1a11ce3
jalafel <jessica.quynh.tran@gmail.com>
2016-03-03 11:16:20 -0500

    fixes date parser

--- public/app/shared/services/dateService.svc.js
+++ public/app/shared/services/dateService.svc.js
@@ -14,14 +14,20 @@
 		this.updateForPost = updateForPost;
 
 		function stringify (date) {
+			if( angular.isString(date) )
+				return parse(date);
 			return $filter('date')(date, "yyyy-MM-dd HH:mm:ss");
+		};
+
+		function parse (date) {
+			return $filter('date')( (new Date(date)), "yyyy-MM-dd HH:mm:ss");
 		};
 
 		function updateForPost (date) {
 			var tempDate = date;
 			date = null;
 			return stringify(date);
-		}
+		};
 
 	}
 
