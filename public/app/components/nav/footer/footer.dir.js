60312d7edf58a22404a57e309811fa3b4b74b937
jalafel <jessica.quynh.tran@gmail.com>
2016-04-28 12:49:33 -0400

    rename templateUrls

--- public/app/components/nav/footer/footer.dir.js
+++ public/app/components/nav/footer/footer.dir.js
@@ -8,9 +8,13 @@
 
 	function footer() {
 
-		
 		return {
-			templateUrl: 'app/shared/nav/footer/footer.tpl.html'
+			template: ['<footer layout="row" layout-align="end end" ng-cloak>',
+    				  		'<md-button class="md-primary md-raised" terms-and-conditions', 
+    				  		'ng-click="ctrl.showTermsAndConditions($event, false)" flex-sm="50" flex-md="25" flex-gt-md="25">',
+        			  		'Terms &amp; Conditions',
+    				  		'</md-button>',
+					    '</footer>'].join('')
 		}
 
 	}
