748ed5a23fc2af3d8815c3d96329e450d6b1c03a
psaunders <psaunders@sosnewmedia.com>
2016-05-08 23:17:34 +0000

    Setting up mail to go to log in testing and by default

--- config/mail.php
+++ config/mail.php
@@ -15,7 +15,7 @@ return [
 	|
 	*/
 
-	'driver' => env('MAIL_DRIVER', 'mailgun'),
+	'driver' => env('MAIL_DRIVER', 'log'),
 
 	/*
 	|--------------------------------------------------------------------------
