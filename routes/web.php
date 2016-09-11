3685c44a334a1c48189d4559069d079df502bb1c
psaunders <psaunders@sosnewmedia.com>
2016-09-11 23:32:35 +0000

    Cleaned out redundant routes

--- routes/web.php
+++ routes/web.php
@@ -27,7 +27,7 @@ Route::get('authenticate/{remember_token}','AuthenticateController@noPassword');
 Route::group(['prefix' => 'api'], function(){				
 	
 	//User
-	Route::resource('user', 'User\UserController');
+	Route::resource('user', 'User\UserController',['except'=>['create','edit']]);
 	Route::resource('user.vote', 'User\UserVoteController',['only'=>['index']]); 
 	Route::resource('user.comment', 'User\UserCommentController',['only'=>['index']]); 
 	Route::resource('user.role', 'User\UserRoleController',['only'=>['index','store','update','destroy']]); 
@@ -38,11 +38,11 @@ Route::group(['prefix' => 'api'], function(){
 
 	//Comment
 	Route::get('comment/{id}/restore','Comment\CommentController@restore'); //Could add deleted status and update it
-	Route::resource('comment', 'Comment\CommentController',['except'=>['store']]);
+	Route::resource('comment', 'Comment\CommentController',['except'=>['store','create','edit']]);
 	Route::resource('comment/{comment}/comment_vote','Comment\CommentCommentVoteController',['only'=>['store']]);
 
 	//Comment Vote
-	Route::resource('comment_vote', 'CommentVote\CommentVoteController',['except'=>['store']]);
+	Route::resource('comment_vote', 'CommentVote\CommentVoteController',['except'=>['store','create','edit']]);
 
 	//Motion
 	Route::resource('motion/{motion}/vote','Motion\MotionVoteController', ['only'=>['index','store']]);
@@ -50,10 +50,10 @@ Route::group(['prefix' => 'api'], function(){
 	Route::get('motion/{id}/restore','Motion\MotionController@restore');
 	//Route::resource('motion.motionfile','Motion\MotionFileController'); //Should be updated in file rework to share same
 	//Route::post('motionfile/flowUpload', 'Motion\MotionFileController@flowUpload');  //Should be updated in file rework to share same
-	Route::resource('motion', 'Motion\MotionController');
+	Route::resource('motion', 'Motion\MotionController',['except'=>['create','edit']]);
 
 	//Page
-	Route::resource('page', 'PageController');
+	Route::resource('page', 'PageController',['except'=>['create','edit']]);
 
 	//Role
 	Route::resource('role', 'RoleController',['only'=>['index']]); //What is this for?
@@ -65,19 +65,19 @@ Route::group(['prefix' => 'api'], function(){
 	Route::group(['middleware' => ['role:administrator']], function(){
 
 		//Community
-		Route::resource('community', 'CommunityController');
+		Route::resource('community', 'CommunityController',['except'=>['create','edit']]);
 
 		//Department
-		Route::resource('department', 'DepartmentController');
+		Route::resource('department', 'DepartmentController',['except'=>['create','edit']]);
 
 		//Ethnic Origin
-		Route::resource('ethnic_origin', 'EthnicOriginController');
+		Route::resource('ethnic_origin', 'EthnicOriginController',['only'=>['index']]);
 
 		//File
 		//Route::resource('file', 'FileController');
 
 		//Background Image
-		Route::resource('background_image', 'BackgroundImageController');
+		Route::resource('background_image', 'BackgroundImageController',['except'=>['create','edit']]);
 	});
 
 
