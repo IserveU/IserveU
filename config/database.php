66d188271a59c42f03457dab7990f519a5e9cc1b
psaunders <psaunders@sosnewmedia.com>
2016-09-11 15:42:29 +0000

    Testing travis

--- config/database.php
+++ config/database.php
@@ -57,7 +57,7 @@ return [
 			'host'      => env('DB_HOST', 'localhost'),
 			'database'  => env('DB_DATABASE', 'forge'),
 			'username'  => env('DB_USERNAME', 'forge'),
-			'password'  => env('DB_PASSWORD', ''),
+			'password'  => env('DB_PASSWORD', 'secret'),
 			'charset'   => 'utf8',
 			'collation' => 'utf8_unicode_ci',
 			'prefix'    => '',
