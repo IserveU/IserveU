ec4ca9703fc7cb3de214cea858acdb9dcdeca3a6
jalafel <jessica.quynh.tran@gmail.com>
2016-09-11 18:37:57 -0400

    add script tags

--- resources/views/index.php
+++ resources/views/index.php
@@ -9,44 +9,36 @@
         <link rel="stylesheet" href="<?=elixir('css/app.css')?>">
         <link rel="icon shortcut" type="image/png" href="<?=Setting::get('logo','default')?>"> 
 
+        <!-- Alloy Editor Dependencies -->
+        <script type="text/javascript" src="https://rawgit.com/liferay/alloy-editor/master/dist/alloy-editor/alloy-editor-all.js"></script>
+        <script src="<?=elixir('js/dependencies.js')?>"></script>
+        <script src="<?=elixir('js/app.js')?>"></script>
+        <script>
+            angular.module("iserveu").constant("CSRF_TOKEN", '<?php echo csrf_token(); ?>');
+        </script>
     </head>
 
-
-    <body set-back-img class="background-image" ng-cloak> 
-
+    <body ng-cloak=""> 
+        <!-- Headers -->
         <beta-message></beta-message>
-
         <user-bar ng-if="!isLoginState"></user-bar>
 
         <div layout="row">
+            <!-- Sidebar -->
+            <md-sidenav ng-if="!isLoginState && settingsGlobal.module.motions"
+                class="site-sidenav md-sidenav-left md-whiteframe-z2 ng-isolate-scope md-closed md-locked-open"
+                role="navigation"
+                md-component-id="left" 
+                md-is-locked-open="$mdMedia('gt-sm')">
+                <motion-sidebar flex></motion-sidebar>
+            </md-sidenav>
 
-        <md-sidenav ng-if="!isLoginState && settingsGlobal.module.motions"
-            class="site-sidenav md-sidenav-left md-whiteframe-z2 ng-isolate-scope md-closed md-locked-open"
-            role="navigation"
-            md-component-id="left" 
-            md-is-locked-open="$mdMedia('gt-sm')">
-            <motion-sidebar flex></motion-sidebar>
-        </md-sidenav>
-
-
-        <md-content id="maincontent" layout-fill
-            ng-style="{'height': isLoginState ? '100vh' : '92vh'}">
-
-                <notification-template ng-hide="isLoginState" flex-order="0"></notification-template>
+            <!-- Main content -->
+            <md-content id="maincontent" layout-fill ng-style="{'height': isLoginState ? '100vh' : '92vh'}">
+                <!-- <notification-template ng-hide="isLoginState" flex-order="0"></notification-template> -->
                 <div ui-view flex flex-order="1" role="main" tabIndex="-1" layout-margin></div>
-                <show-footer flex-order="2" layout-margin flex="noshrink"></show-footer>            
-
-        </md-content>
-
+                <show-footer flex-order="2" layout-margin flex="noshrink"></show-footer>
+            </md-content>
         </div>
     </body>        
-
-
-    <script src="<?=elixir('js/dependencies.js')?>"></script>
-    <script src="<?=elixir('js/app.js')?>"></script>
-
-    <script>
-        angular.module("iserveu").constant("CSRF_TOKEN", '<?php echo csrf_token(); ?>');
-    </script>
-    
 </html> 
