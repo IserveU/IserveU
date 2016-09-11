85038f5a38b0a9d1786e2206afaa421d8cbe2f06
psaunders <psaunders@sosnewmedia.com>
2016-09-11 16:13:15 +0000

    Checking if JSON support is real issue

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -37,7 +37,7 @@ class CreateUsersTable extends Migration {
             $table->date('address_verified_until')->nullable();
 			$table->boolean('identity_verified')->default(0);
 
-            $table->json('preferences')->nullable();
+            $table->string('preferences')->nullable();
 			$table->integer('login_attempts')->default(0);
 			$table->datetime('locked_until')->nullable();
 
