1cdb6072b7ce7e653aeefd978c0eae2025409031
jessicaquynh <jessica.quynh.tran@gmail.com>
2016-10-20 16:39:03 -0400

    eslint models

--- public/app/models/commentVoteModel.svc.js
+++ public/app/models/commentVoteModel.svc.js
@@ -1,133 +1,132 @@
-(function() {
-	
 'use strict';
+(function(window, angular, undefined) {
 
-angular
-.module('iserveu')
-.factory('CommentVote', [
-	'commentVoteResource',
 
-function(commentVoteResource) {
+  angular
+    .module('iserveu')
+    .factory('CommentVote', [
+      'commentVoteResource',
+      CommentVoteFactory]);
 
-	function CommentVote(commentVoteData) {
+  function CommentVoteFactory(commentVoteResource) {
 
-		if(commentVoteData) {
-			this.setData(commentVoteData);
-		}
+    /** Constructor */
+    function CommentVote(commentVoteData) {
+      if (commentVoteData) {
+        this.setData(commentVoteData);
+      }
 
-		this.agree = {
-			type: 'agree',
-			value: 1,
-			selectedIcon: 'thumb-up-outline',
-			class: 'md-primary',
-			activeIcon: 'thumb-up',
-			defaultIcon: 'thumb-up-outline',
-			isActive: false
-		};
+      this.agree = {
+        type: 'agree',
+        value: 1,
+        selectedIcon: 'thumb-up-outline',
+        class: 'md-primary',
+        activeIcon: 'thumb-up',
+        defaultIcon: 'thumb-up-outline',
+        isActive: false
+      };
 
-		this.disagree = {
-			type: 'disagree',
-			value: -1,
-			selectedIcon: 'thumb-down-outline',
-			class: 'md-accent',
-			activeIcon: 'thumb-down',
-			defaultIcon: 'thumb-down-outline',
-			isActive: false
-		};
-	}
+      this.disagree = {
+        type: 'disagree',
+        value: -1,
+        selectedIcon: 'thumb-down-outline',
+        class: 'md-accent',
+        activeIcon: 'thumb-down',
+        defaultIcon: 'thumb-down-outline',
+        isActive: false
+      };
+    }
 
-	CommentVote.prototype = {
+    CommentVote.prototype = {
 
-		setData: function(commentVoteData) {
-			angular.extend(this, commentVoteData);
-		},
+      setData: function(commentVoteData) {
+        angular.extend(this, commentVoteData);
+      },
 
-		setActive: function(type) {
-			type = stringifyPosition(type);
-			this[type].isActive = true;
-			this[type].selectedIcon = this[type].activeIcon;
-		},
+      setActive: function(type) {
+        type = stringifyPosition(type);
+        this[type].isActive = true;
+        this[type].selectedIcon = this[type].activeIcon;
+      },
 
-		setDefault: function(type) {
-			type = stringifyPosition(type);
-			this[type].isActive = false;
-			this[type].selectedIcon = this[type].defaultIcon;
-		},
+      setDefault: function(type) {
+        type = stringifyPosition(type);
+        this[type].isActive = false;
+        this[type].selectedIcon = this[type].defaultIcon;
+      },
 
-		castVote: function(comment_id, pos) {
+      castVote: function(comment_id, pos) {
 
-			var type    = stringifyPosition( pos );
-			var oldType = stringifyPosition( pos *= -1 );
+        var type = stringifyPosition(pos);
+        var oldType = stringifyPosition(pos *= -1);
 
-			if( this[type].isActive ) {
-				destroy(this, type);
-			} else if( this[oldType].isActive && this.id  ) {
-				update( this, type, oldType );
-			} else {
-				create( this, type, comment_id );
-			}
-		}
-	}
+        if (this[type].isActive) {
+          destroy(this, type);
+        } else if (this[oldType].isActive && this.id) {
+          update(this, type, oldType);
+        } else {
+          create(this, type, comment_id);
+        }
+      }
+    };
 
     /*****************************************************************
     *
-    *	Private functions.
+    * Private functions.
     *
     ******************************************************************/
 
     function numberifyPosition(pos) {
-    	if(!angular.isString(pos) || pos.length <= 2) {
-			return +pos;
-		}
-		return pos == 'agree' ? 1 : -1;
+      if (!angular.isString(pos) || pos.length <= 2) {
+        return +pos;
+      }
+      return pos == 'agree' ? 1 : -1;
     }
 
-	function stringifyPosition(pos) {
-		if(angular.isString(pos) && pos.length > 2) {
-			return pos;
-		}
-		return pos === 1 ? 'agree' : 'disagree';
-	}
+    function stringifyPosition(pos) {
+      if (angular.isString(pos) && pos.length > 2) {
+        return pos;
+      }
+      return pos === 1 ? 'agree' : 'disagree';
+    }
 
-	function create(self, type, comment_id) {
-		self.setActive(type); // either put here or above.
-		commentVoteResource.saveCommentVote({
-			comment_id: comment_id,
-			position: numberifyPosition(type)
-		}).then(function(success){
-			self.id  = success.id;
-		}, function(error){
-			self.setDefault(type);
-		});
-	}
+    function create(self, type, comment_id) {
+      self.setActive(type); // either put here or above.
+      commentVoteResource.saveCommentVote({
+        comment_id: comment_id,
+        position: numberifyPosition(type)
+      }).then(function(success) {
+        self.id = success.id;
+      }, function(error) {
+        self.setDefault(type);
+      });
+    }
 
-	function update(self, type, oldType) {
+    function update(self, type, oldType) {
 
-		self.setActive(type);
-		self.setDefault(oldType);
+      self.setActive(type);
+      self.setDefault(oldType);
 
-		commentVoteResource.updateCommentVote({
-			id: self.id,
-			position: numberifyPosition(type)
-		}).then(function(success){}, function(error){
-			self.setDefault(type);
-		});
-	}
+      commentVoteResource.updateCommentVote({
+        id: self.id,
+        position: numberifyPosition(type)
+      }).then(function(success) {},
+        function(error) {
+          self.setDefault(type);
+        });
+    }
 
-	function destroy(self, type) {
-		self.setDefault(type);
-		commentVoteResource.deleteCommentVote({id: self.id}).then(function(){
-			delete self.id;
-		}, function(error){
-			self.setActive(type);
-		});
-	}
+    function destroy(self, type) {
+      self.setDefault(type);
+      commentVoteResource.deleteCommentVote({id: self.id}).then(function() {
+        delete self.id;
+      }, function(error) {
+        self.setActive(type);
+      });
+    }
 
+    /** Return contructor */
+    return CommentVote;
+  }
 
-	return CommentVote;
-
-}]);
-
-
-})();
-
+})(window, window.angular);
--- public/app/models/motionVotesModel.svc.js
+++ public/app/models/motionVotesModel.svc.js
@@ -1,120 +1,131 @@
-(function() {
-	
-angular
-	.module('iserveu')
-	.factory('MotionVotes', [
-		'$translate',
-		'motionVoteStatusbarService',
-	function($translate, motionVoteStatusbarService) {
+'use strict';
+(function(window, angular, undefined) {
 
-		function MotionVotes(motionVoteData) {
+  angular
+    .module('iserveu')
+    .factory('MotionVotes', [
+      '$translate',
+      'motionVoteStatusbarService',
+      MotionVotesFactory]);
 
-			// I wonder if this is something i should be doing,
-			// because ... one of hte pros of using models
-			// is to have contracts; so if something doesn't
-			// exist then to destroy it?
-			if(motionVoteData) {
-				this.setData(motionVoteData);
-			} 
+  function MotionVotesFactory($translate, motionVoteStatusbarService) {
 
-			this.overallPosition = {};
-		}
+    function MotionVotes(motionVoteData) {
 
-		var overallPosition = {
-			default: {
-				icon: 'thumbs-up-down',
-				message: 'There have been no votes'
-			},
-			loading: {
-				icon: 'loading',
-				message: ''
-			},
-			agree: {
-				icon: 'thumb-up',
-				message: 'Most people agree'
-			},
-			disagree: {
-				icon: 'thumb-down',
-				message: 'Most people disagree'
-			},
-			abstain: {
-				icon: 'thumbs-up-down',
-				message: 'Most people abstained'
-			},
-			tie: {
-				icon: 'thumbs-up-down',
-				message: 'There has been a majority tie'
-			}
-		}
+      // I wonder if this is something i should be doing,
+      // because ... one of hte pros of using models
+      // is to have contracts; so if something doesn't
+      // exist then to destroy it?
+      if (motionVoteData) {
+        this.setData(motionVoteData);
+      }
 
-		MotionVotes.prototype = {
-			setData: function(motionVoteData) {
-				angular.extend(this, motionVoteData);
-				motionVoteStatusbarService.setStatusbar(this);
-			},
+      this.overallPosition = {};
+    }
 
-			setOverallPosition: function(overallPosition) {
-				console.log(overallPosition);
-				this.setData({overallPosition: overallPosition});
-			},
+    var overallPosition = {
+      default: {
+        icon: 'thumbs-up-down',
+        message: 'There have been no votes'
+      },
+      loading: {
+        icon: 'loading',
+        message: ''
+      },
+      agree: {
+        icon: 'thumb-up',
+        message: 'Most people agree'
+      },
+      disagree: {
+        icon: 'thumb-down',
+        message: 'Most people disagree'
+      },
+      abstain: {
+        icon: 'thumbs-up-down',
+        message: 'Most people abstained'
+      },
+      tie: {
+        icon: 'thumbs-up-down',
+        message: 'There has been a majority tie'
+      }
+    };
 
-			setOverallPositionLoading: function() {
-	            this.setData({ overallPosition: overallPosition.loading });	
-			},
+    MotionVotes.prototype = {
+      setData: function(motionVoteData) {
+        angular.extend(this, motionVoteData);
+        motionVoteStatusbarService.setStatusbar(this);
+      },
 
-			getOverallPosition: function() {
-				extractData(this);
-			},
+      setOverallPosition: function(overallPosition) {
+        this.setData({overallPosition: overallPosition});
+      },
 
-			reload: function(motionVoteData) {
-				// resets motion vote data
-				angular.extend(this, {abstain: null, agree: null, disagree: null});
-				this.setData(motionVoteData);
-				return this;
-			}
-		}
+      setOverallPositionLoading: function() {
+        this.setData({ overallPosition: overallPosition.loading });
+      },
 
-		function extractData(votes) {
-			var _votes = votes.data || votes;
+      getOverallPosition: function() {
+        extractData(this);
+      },
 
-			function parse(key, type){
-				return ( key && key.active && key.active.number ) ? key.active.number : 0;
-			}
-			// using this for the time being
-			var _d = angular.extend({}, {
-				abstain:  parse(_votes.abstain),
-				agree:    parse(_votes.agree),
-				disagree: parse(_votes.disagree)
-			});
+      reload: function(motionVoteData) {
+        // resets motion vote data
+        angular.extend(this, {abstain: null, agree: null, disagree: null});
+        this.setData(motionVoteData);
+        return this;
+      }
+    };
 
-			// literally every permutation ... hopefully.
-			if ( _d.abstain === _d.agree && _d.abstain > _d.disagree || 
-						_d.abstain === _d.disagree && _d.abstain > _d.agree    ||
-						_d.agree   === _d.disagree && _d.agree > _d.abstain ||
-						( _d.agree === _d.disagree && _d.agree === _d.abstain && _d.agree !== null) ) {
-				 votes.setOverallPosition( overallPosition.tie );
-				return 'tie';
-			} else if ( !_d.abstain && !_d.agree && !_d.disagree ) {
-				 votes.setOverallPosition( overallPosition.default );
-				return 'no votes';
-			} else if( _d.disagree && ( !_d.agree || !_d.abstain ) ||
-				_d.disagree > _d.agree && _d.disagree > _d.abstain ) {
-	            votes.setOverallPosition( overallPosition.disagree );
-				return 'disagree';
-			} else if (  _d.agree && ( !_d.disagree || !_d.abstain ) ||
-				_d.agree > _d.disagree && _d.agree > _d.abstain  ) {
-			    votes.setOverallPosition( overallPosition.agree );
-				return 'agree'
-			} else if (  _d.abstain && ( !_d.disagree || !_d.agree ) ||
-				_d.abstain > _d.disagree && _d.abstain > _d.agree  ) {
-	            votes.setOverallPosition( overallPosition.abstain );
-				return 'abstain';
-			} 
-		}
+    function extractData(votes) {
+      var _votes = votes.data || votes;
 
-		return MotionVotes;
+      function parse(key, type) {
+        if (key && key.active && key.active.number) {
+          return key.active.number;
+        }
+        return 0;
+      }
 
-}])
+      // using this for the time being
+      var _d = angular.extend({}, {
+        abstain:  parse(_votes.abstain),
+        agree:    parse(_votes.agree),
+        disagree: parse(_votes.disagree)
+      });
 
-})();
+      // literally every permutation ... hopefully.
+      if (_d.abstain === _d.agree && _d.abstain > _d.disagree ||
+          _d.abstain === _d.disagree && _d.abstain > _d.agree ||
+          _d.agree === _d.disagree && _d.agree > _d.abstain ||
+          (_d.agree === _d.disagree && (_d.agree === _d.abstain) &&
+          _d.agree !== null)) {
 
+        votes.setOverallPosition(overallPosition.tie);
+        return 'tie';
+      } else if (!_d.abstain && !_d.agree && !_d.disagree) {
+
+        votes.setOverallPosition(overallPosition.default);
+        return 'no votes';
+      } else if (_d.disagree && (!_d.agree || !_d.abstain) ||
+        _d.disagree > _d.agree && _d.disagree > _d.abstain) {
+
+        votes.setOverallPosition(overallPosition.disagree);
+        return 'disagree';
+      } else if (_d.agree && (!_d.disagree || !_d.abstain) ||
+        _d.agree > _d.disagree && _d.agree > _d.abstain) {
+
+        votes.setOverallPosition(overallPosition.agree);
+        return 'agree';
+      } else if (_d.abstain && (!_d.disagree || !_d.agree) ||
+        _d.abstain > _d.disagree && _d.abstain > _d.agree) {
+
+        votes.setOverallPosition(overallPosition.abstain);
+        return 'abstain';
+      }
+    }
+
+    return MotionVotes;
+
+  }
+
+})(window, window.angular);
