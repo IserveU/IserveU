1078376461d863c7af0195a0d88306a0d5d5f1b5
jessicaquynh <jessica.quynh.tran@gmail.com>
2016-10-27 16:47:13 -0400

    adds an extra function to spinner to trigger a function when completed

--- public/app/shared/directives/spinner.dir.js
+++ public/app/shared/directives/spinner.dir.js
@@ -3,29 +3,51 @@
 
   angular
     .module('iserveu')
-    .directive('spinner', ['settings', spinner]);
+    .directive('spinner', ['settings', '$timeout', spinner]);
 
      /** @ngInject */
-  function spinner(settings) {
+  function spinner(settings, $timeout) {
 
     // TODO: make loading circle changeable from settings array
 
     return {
       restrict: 'E',
       transclude: true,
+      link: function(scope, el, attrs) {
+
+        scope.onFinish = false;
+
+        function flipOnFinish() {
+          scope.onFinish = !scope.onFinish;
+        }
+
+        scope.$watch('onHide', function(oldValue, newValue) {
+          if (oldValue == false && newValue == true) {
+            flipOnFinish();
+            $timeout(function() {
+              flipOnFinish();
+              scope.onLoaded();
+            }, 1400);
+          }
+        }, true);
+      },
       scope: {
         'name': '=',
         'onLoaded': '&',
         'primaryAction': '&',
         'onHide': '=',
         'formName': '=',
-        'secondaryBtn': '='
+        'secondaryBtn': '=',
       },
-      template: ['<md-button ng-disabled="formName.$invalid" type="submit" ',
-        'ng-click="primaryAction()">',
-        '<span ng-hide="onHide">{{name}}</span>',
-        '<md-icon md-svg-src="/themes/default/loading.svg" ng-show="onHide">',
-        '</md-icon></md-button>',
+      template: [
+        '<md-button ng-disabled="formName.$invalid" type="submit" ng-click="primaryAction()">',
+
+          '<md-icon class="mdi mdi-check" style="margin-bottom: 14px; margin-right: 5px" ng-show="onFinish"></md-icon>',
+
+          '<span ng-hide="onHide">{{name}}</span>',
+
+          '<md-icon md-svg-src="/themes/default/loading.svg" ng-show="onHide"></md-icon>',
+        '</md-button>',
         '<md-button ng-if="secondaryBtn" ng-click="secondaryBtn.control()">',
         '{{secondaryBtn.name}}',
         '</md-button>'
