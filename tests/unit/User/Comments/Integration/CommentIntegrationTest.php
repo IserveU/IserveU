0daf5c5c226ae7a837760d43381dd50d7f95f798
vinhtruong94 <vinhtruong94@users.noreply.github.com>
2016-11-06 14:40:10 -0500

    adding integration test for comment (#463)
    * adding integration test for comment
    
    * Applied fixes from StyleCI (#469)

---
+++ tests/unit/User/Comments/Integration/CommentIntegrationTest.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Foundation\Testing\DatabaseTransactions;
+
+class CommentIntegrationTest extends TestCase
+{
+    use DatabaseTransactions;
+
+
+    protected static $commentingUser;
+
+    public function setUp()
+    {
+        parent::setUp();
+    }
+
+    /** @test */
+    public function user_can_see_comments_after_commenting()
+    {
+        // sign in
+        $user = factory(App\User::class, 'verified')->create();
+        $user->addRole('citizen');
+        $this->signIn($user);
+
+        $motion = factory(App\Motion::class, 'published')->create();
+        $this->post('/api/motion/'.$motion->slug.'/vote', ['position' => 1])->assertResponseStatus(200);
+        $this->post('/api/vote/'.$motion->votes()->first()->id.'/comment', ['text' => 'testing testing testing']);
+        $this->visit('/api/motion/'.$motion->slug.'/comment')->see('testing testing testing');
+    }
+}
