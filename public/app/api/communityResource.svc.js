dd581f22871e3ef65ac090eed79df1300e273388
Jessica Quynh Tran <jessica.quynh.tran@gmail.com>
2016-11-27 01:59:45 -0500

    convert to using cacheFactory

--- public/app/api/communityResource.svc.js
+++ public/app/api/communityResource.svc.js
@@ -5,28 +5,24 @@
     .module('iserveu')
     .factory('communityResource', [
       '$http',
+      '$cacheFactory',
       '$q',
       'utils',
       communityResource]);
 
-  function communityResource($http, $q, utils) {
-    var index = {};
+  function communityResource($http, $cacheFactory, $q, utils) {
 
     /**
      * Get all communities
      * @return {promise} promise or cached index
      */
     function getCommunities() {
-      if (!utils.objectIsEmpty(index)) {
-        return $q.when({data: index});
-      }
-
       return $http({
         method: 'GET',
         url: '/api/community',
+        cache: true,
         ignoreLoadingBar: true
       }).success(function(results) {
-        index = results;
         return results;
       }).error(function(error) {
         return error;
@@ -34,31 +30,37 @@
     }
 
     /**
-     * grab community given the id
+     * Grab the community given the id
      * @param  {number} id
      * @return {promise}    promise or cached item
      */
     function retrieveNameById(id) {
-      if (utils.objectIsEmpty(index)) {
+
+      var communities = $cacheFactory.get('$http').get('/api/community'),
+          name;
+
+      if (angular.isUndefined(communities)) {
         return getCommunities().then(function(results) {
           return retrieveNameById(id);
         });
       } else {
-        var name;
-        index.forEach(function(el) {
-          if (el.id === id) {
-            name = el.name;
-          }
+        // parse string to json
+        communities = JSON.parse(communities[1]);
+
+        communities.forEach(function(community) {
+          if (community.id === id)
+            name = community.name;
         });
 
+
+        console.log(name);
         return $q.when(name);
       }
     }
 
     return {
       getCommunities: getCommunities,
-      retrieveNameById: retrieveNameById,
-      index: index
+      retrieveNameById: retrieveNameById
     };
   }
 })(window, window.angular);
