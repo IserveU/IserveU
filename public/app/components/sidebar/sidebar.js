29860a49a71234329fd312110576d0f402166b22
Kiniamaro <vdyotte@gmail.com>
2017-01-06 15:02:10 -0500

    switch sidebars on specific routes

--- public/app/components/sidebar/sidebar.js
+++ public/app/components/sidebar/sidebar.js
@@ -6,14 +6,17 @@ angular
 
   function sidebar() {
 
-    function sidebarController() {
-
-      // pass
-
+    function SidebarController($scope, $location) {
+      $scope.location = $location.path();
+      $scope.$on('$locationChangeStart', function () {
+        $scope.location = $location.path();
+      });
     }
 
+    SidebarController.$inject = ['$scope', '$location'];
+
     return {
-      controller: sidebarController,
+      controller: SidebarController,
       controllerAs: 'sidebar',
       templateUrl: 'app/components/sidebar/sidebar.tpl.html'
     }
