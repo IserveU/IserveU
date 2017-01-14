5a7622e88c345ba00f8267ecef583fec188e11b9
Victor Dyotte <vdyotte@gmail.com>
2017-01-14 15:01:23 -0500

    Vote tile fixes (#596)
    * move vote position logic into tile controller
    
    * Fix bug if user has not voted
    
    * Apply fixes from StyleCI
    
    * Put in enhanced protractor checks

--- app/Http/Controllers/User/UserVoteController.php
+++ app/Http/Controllers/User/UserVoteController.php
@@ -5,7 +5,6 @@ namespace App\Http\Controllers\User;
 use App\Http\Controllers\ApiController;
 use App\User;
 use App\Vote;
-use Auth;
 use Illuminate\Support\Facades\Request;
 
 class UserVoteController extends ApiController
@@ -19,12 +18,6 @@ class UserVoteController extends ApiController
     {
         $limit = Request::get('limit') ?: 100;
 
-        if (Auth::user()->id != $user->id && !$user->public && !Auth::user()->can('show-vote')) { //Not the current user, or public and not an admin
-             abort(401, "You do not have permission to view this non-public user's votes");
-        }
-
-        $votes = Vote::where('user_id', $user->id)->paginate($limit);
-
-        return $votes;
+        return Vote::where('user_id', $user->id)->paginate($limit);
     }
 }
--- public/app/components/motionTiles/motionTiles.js
+++ public/app/components/motionTiles/motionTiles.js
@@ -1,24 +1,55 @@
 (function() {
-	
-	angular
-		.module('iserveu')
-		.directive('motionTiles', motionTiles);
 
-	function motionTiles() {
+  angular
+    .module('iserveu')
+    .directive('motionTiles', [ 'Motion', motionTiles ]);
 
-		function motionTilesController($scope, $mdMedia) {
-			
-			$scope.direction = $mdMedia('gt-sm') ? 'left' : '';
+    function motionTiles() {
 
-		}
+    function motionTilesController($scope, $mdMedia, Motion) {
 
-		return {
-			controller: ['$scope', '$mdMedia', motionTilesController],
-			controllerAs: 'motionTiles',
-			templateUrl: 'app/components/motionTiles/motionTiles.tpl.html'
-		}
+      $scope.getRankTile = function(rank) {
+        var overallPosition = {
+          loading: {
+            icon: 'loading',
+            message: ''
+          },
+          agree: {
+            icon: 'thumb-up',
+            message: 'Majority agree'
+          },
+          disagree: {
+            icon: 'thumb-down',
+            message: 'Majority disagree'
+          },
+          tie: {
+            icon: 'thumbs-up-down',
+            message: 'Majority tie'
+          }
+        };
+
+        if (rank < 0 )
+          return overallPosition.disagree;
+
+        if (rank > 0)
+          return overallPosition.agree;
+
+        if (rank === 0)
+          return overallPosition.tie;
+
+        return overallPosition.loading;
+      }
+
+      $scope.direction = $mdMedia('gt-sm') ? 'left' : '';
+    }
+
+    return {
+      controller: ['$scope', '$mdMedia', motionTilesController],
+      controllerAs: 'motionTiles',
+      templateUrl: 'app/components/motionTiles/motionTiles.tpl.html'
+    }
 
 
-	}
+  }
 
 })();
