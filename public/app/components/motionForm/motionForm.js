e42b80cb7c77288c278b5a2e803d5ef9d6c454bb
psaunders <psaunders@sosnewmedia.com>
2017-05-24 00:34:47 -0400

    Seeding Fixes (#775)
    * Removed motion route renaming
    
    * Fix issue with template url
    
    * Apply fixes from StyleCI

--- public/app/components/motionForm/motionForm.js
+++ public/app/components/motionForm/motionForm.js
@@ -1,143 +1,160 @@
 (function() {
 
-	'use strict';
+  'use strict';
 
-	angular
-		.module('iserveu')
-		.directive('motionForm', [
-			'$rootScope',
-			'$state',
-			'$stateParams',
-			'$timeout',
-			'$translate',
-			'isuSectionProvider',
-			'Motion',
-			'motionResource',
-			'motionFileResource',
-			'fileResource',
-			'ToastMessage',
-			'motionDepartments',
-			'motionFilesFactory',
-			'Authorizer',
-		motionForm]);
+  angular
+    .module('iserveu')
+    .directive('motionForm', [
+      '$rootScope',
+      '$state',
+      '$stateParams',
+      '$timeout',
+      '$translate',
+      'isuSectionProvider',
+      'Motion',
+      'motionResource',
+      'motionFileResource',
+      'fileResource',
+      'ToastMessage',
+      'motionDepartments',
+      'motionFilesFactory',
+      'Authorizer',
+      motionForm
+    ]);
 
-	function motionForm($rootScope, $state, $stateParams, $timeout, $translate, isuSectionProvider, Motion, motionResource, motionFileResource, fileResource,
-		ToastMessage, motionDepartments, motionFilesFactory, Authorizer) {
+  function motionForm($rootScope, $state, $stateParams, $timeout, $translate, isuSectionProvider, Motion, motionResource, motionFileResource, fileResource,
+    ToastMessage, motionDepartments, motionFilesFactory, Authorizer) {
 
-		function motionFormController($scope) {
-			var self = this;
+    function motionFormController($scope) {
+      var self = this;
 
-			self.createMotion = $state.current.name === 'create-motion' ? true : false;
-			self.departments = motionDepartments;
-			self.existingMotionFiles = [];
-	    self.motion = new Motion({ closing_at: new Date(+new Date + 12096e5), status: 'draft' });
-			self.motionFile  = motionFilesFactory;
-			self.motionFiles = [];
-			self.processing = false;
-			self.cancel = cancel;
-			self.triggerSpinner = triggerSpinner;
-			self.successHandler = successHandler;
+      self.createMotion = $state.current.name === 'create-motion' ? true : false;
+      self.departments = motionDepartments;
+      self.existingMotionFiles = [];
+      self.motion = new Motion({
+        closing_at: new Date(+new Date + 12096e5),
+        status: 'draft'
+      });
+      self.motionFile = motionFilesFactory;
+      self.motionFiles = [];
+      self.processing = false;
+      self.cancel = cancel;
+      self.triggerSpinner = triggerSpinner;
+      self.successHandler = successHandler;
 
       function cancel() {
-      	ToastMessage.cancelChanges(function() {
+        ToastMessage.cancelChanges(function() {
           if (self.createMotion) {
             navigateAway();
           } else {
-            $state.go('motion', {id: self.motion.id});
+            $state.go('motion', {
+              id: self.motion.id
+            });
           }
-    		});
-  		  var navigateAway = function() {
+        });
+        var navigateAway = function() {
           motionResource.deleteMotion($stateParams.id);
-    			$state.go('dashboard');
-    		};
+          $state.go('dashboard');
+        };
       }
 
       function successHandler(r) {
-    	  /** deprecrated */
+        /** deprecrated */
         // motionFilesFactory.attach(r.id, self.motionFiles);
 
         $rootScope.preventStateChange = true;
         self.motion.setData(r).refreshExtensions();
 
-        if(self.motion.id) {
-        	ToastMessage.simple("You successfully updated this " + $translate.instant('MOTION'));
-        } else if(Authorizer.canAccess('edit-motion')){
-        	ToastMessage.simple("Your submission has been sent in for review!");
+        if (self.motion.id) {
+          ToastMessage.simple("You successfully updated this " + $translate.instant('MOTION'));
+        } else if (Authorizer.canAccess('edit-motion')) {
+          ToastMessage.simple("Your submission has been sent in for review!");
         }
 
         $timeout(function() {
-         	$state.go( 'motion', ( {id: r.id} ), {reload: true} );
+          $state.go('motion', ({
+            id: r.id
+          }), {
+            reload: true
+          });
         }, 600);
       }
 
-			function triggerSpinner(val) {
-				self.processing = val || !self.processing;
-			}
+      function triggerSpinner(val) {
+        self.processing = val || !self.processing;
+      }
 
-			/** Initializing function to get motion data. */
-			(function init() {
+      /** Initializing function to get motion data. */
+      (function init() {
 
         motionDepartments.loadAll();
 
-				// if edit-motion
-				if (self.createMotion)
-					return false;
+        // if edit-motion
+        if (self.createMotion)
+          return false;
 
-				self.motion = Motion.get($stateParams.id);
-				fileResource.getFiles($stateParams.id).then(function(r){
-					self.existingMotionFiles = r;
-				});
+        self.motion = Motion.get($stateParams.id);
+        fileResource.getFiles($stateParams.id).then(function(r) {
+          self.existingMotionFiles = r;
+        });
 
-			})();
-		}
+      })();
+    }
 
-		function motionFormLink(scope, el, attrs, ctrl) {
+    function motionFormLink(scope, el, attrs, ctrl) {
 
-			var autopost = !!angular.element(el).attr('autopost');
+      var autopost = !!angular.element(el).attr('autopost');
 
-			if (autopost) {
-				angular.extend(isuSectionProvider.defaults, {target: '/api/motion', method: 'POST'});
-				var motion = ctrl.motion._sanitize();
-				isuSectionProvider.callMethodToApi(motion).then(function(success){
-					$stateParams.id = success.id;
-					self.motion = new Motion(success);
-				}, function(error){
-				});
-			}
+      if (autopost) {
+        angular.extend(isuSectionProvider.defaults, {
+          target: '/api/motion',
+          method: 'POST'
+        });
 
-			el.bind('submit', function(ev){
-				ev.preventDefault();
-				saveMotion();
-			});
+        var motion = ctrl.motion._sanitize();
+        isuSectionProvider.callMethodToApi(motion).then(function(success) {
+          $stateParams.id = success.id;
+          self.motion = new Motion(success);
+        }, function(error) {});
+      }
 
-			function saveMotion() {
-				ctrl.triggerSpinner(true);
+      el.bind('submit', function(ev) {
+        ev.preventDefault();
+        saveMotion();
+      });
 
-				angular.extend(isuSectionProvider.defaults,
-					(!$stateParams.id) ?
-					{target: '/api/motion', method: 'POST'} :
-					{target: '/api/motion/' + $stateParams.id, method: 'PATCH'});
+      function saveMotion() {
+        ctrl.triggerSpinner(true);
 
-				var motion = ctrl.motion._sanitize();
+        angular.extend(isuSectionProvider.defaults,
+          (!$stateParams.id) ? {
+            target: '/api/motion',
+            method: 'POST'
+          } : {
+            target: '/api/motion/' + $stateParams.id,
+            method: 'PATCH'
+          });
 
-				isuSectionProvider.callMethodToApi(motion).then(function(success){
-					ctrl.triggerSpinner(false);
-					ctrl.successHandler(success);
-				}, function(error){
-					ctrl.triggerSpinner(false);
-				});
-			}
-		}
+        var motion = ctrl.motion._sanitize();
 
-		return {
-			link: motionFormLink,
-			controller: ['$scope', motionFormController],
-			controllerAs: 'form',
-			template: 'app/components/motionForm/motionForm.tpl.html'
-		}
+        isuSectionProvider.callMethodToApi(motion).then(function(success) {
+          ctrl.triggerSpinner(false);
+          ctrl.successHandler(success);
+        }, function(error) {
+          ctrl.triggerSpinner(false);
+        });
+      }
+    }
+
+    return {
+      link: motionFormLink,
+      controller: ['$scope', motionFormController],
+      controllerAs: 'form',
+      templateUrl: 'app/components/motionForm/motionForm.tpl.html'
+    }
 
 
-	}
+  }
 
 
 })();
